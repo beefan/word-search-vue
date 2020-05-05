7331293d4d4c11815c9d2a56873a10769bcc3c57
Brandon Fannin <bfannin13@gmail.com>
2020-05-05 15:10:45 -0400

    added size of 5 as a radio button option

--- src/components/WordSearch.vue
+++ src/components/WordSearch.vue
@@ -23,6 +23,7 @@ export default {
     return {
       size: 10,
       gridOptions: [
+          { text: '5', value: 5 },
           { text: '10', value: 10 },
           { text: '15', value: 15 },
           { text: '20', value: 20 },
